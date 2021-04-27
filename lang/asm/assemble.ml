994555ec6f9030222aa2e643d82ea1aeb9e0d24b
Thomas Castleman <thomascastleman0@gmail.com>
2021-04-27 15:54:18 -0400

    ideas about assembler, and add testing framework

--- lang/asm/assemble.ml
+++ lang/asm/assemble.ml
@@ -1,5 +1,48 @@
 open Isa
 
+(* [bytes_from_list] constructs a buffer of raw bytes from
+  the given list of integers *)
+let bytes_from_list (l : int list) : bytes =
+  let buf = Bytes.create (List.length l) in
+  List.mapi (fun i b -> Bytes.set_int8 buf i b) l |> ignore;
+  buf
+
+(* [assemble_to_buf] converts the given instructions to a list of 
+  integers representing the assembled bytes of the program *)
+let assemble_to_buf (_instrs : instr list)
+    (_label_map : (string, int) Hashtbl.t) : int list =
+  []
+
+(* [map_labels] constructs a mapping from label names to memory addresses *)
+let map_labels (_instrs : instr list) = Hashtbl.create 0
+(* 
+label_to_addr = {}
+byte_pos = 0
+pass over instrs:
+  if Label _:
+    label_to_addr[label] = byte_pos
+  otherwise:
+    byte_pos += sizeof(ins)
+ *)
+
+(* [size_of] determines the size (in bytes) of the given instruction *)
+let size_of (ins : instr) : int =
+  match ins with
+  (* labels don't appear in assembled program *)
+  | Label _ -> 0
+  (* one-byte instructions *)
+  | Add _ | Sub _ | And _ | Or _ | Xor _ | Mov _ | Ld _ | St _ | Cmp _ | Not _
+  | Inr _ | Dcr _ | Ret | Hlt | Nop ->
+      1
+  (* two-byte instructions *)
+  | Addi _ | Subi _ | Ani _ | Ori _ | Xri _ | Mvi _ | Lds _ | Sts _ | Jmp _
+  | Je _ | Jne _ | Jg _ | Jge _ | Jl _ | Jle _ | Call _ | Dic _ | Did _ | Out _
+  | Cmpi _ ->
+      2
+
 (* [assemble] processes a list of asm instructions and 
   produces a byte sequence representing the program in binary form *)
-let assemble (_instrs : instr list) : bytes = Bytes.create 0
+let assemble (instrs : instr list) : bytes =
+  let label_map = map_labels instrs in
+  let bytes_as_list = assemble_to_buf instrs label_map in
+  bytes_from_list bytes_as_list
--- lang/test/test_assembler.ml
+++ lang/test/test_assembler.ml
@@ -1,7 +1,12 @@
 open OUnit2
+open Asm.Assemble
+open Asm.Isa
 
-let test_it_works _ = assert_equal (1 + 1) 2
+let test_simple_pgrm _ =
+  let instrs = [ Mvi (5, A); Dcr A; Hlt ] in
+  let bin = bytes_from_list [ 0x49; 0x05; 0x3f; 0x7e ] in
+  assert_equal (assemble instrs) bin
 
-let suite = "Suite1" >::: [ "test_it_works" >:: test_it_works ]
+let suite = "Suite1" >::: [ "test_simple_pgrm" >:: test_simple_pgrm ]
 
 let () = run_test_tt_main suite
