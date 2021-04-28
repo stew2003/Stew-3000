f36695921b33a978dbf3e57dc93a935707981242
stew2003 <stewshadow@gmail.com>
2021-04-28 10:20:24 -0400

    fiexed a slight bug

--- RAM_loader/RAM_loader.py
+++ RAM_loader/RAM_loader.py
@@ -2,29 +2,28 @@ import serial
 import time
 import sys
 
-if __name__ == "main":
-    # set up the serial connection to the arduino board
-    serial_port = "/dev/ttyACM0"
-    baud_rate = "9600"
+# set up the serial connection to the arduino board
+serial_port = "/dev/ttyACM0"
+baud_rate = "9600"
 
-    connection = serial.Serial(serial_port, baud_rate, timeout=5)
+connection = serial.Serial(serial_port, baud_rate, timeout=5)
 
-    time.sleep(2) # wait for arduino
+time.sleep(2) # wait for arduino
 
-    with open(sys.argv[0], "rb") as file:
+with open(sys.argv[1], "rb") as file:
+    print("hello world")
+    byte = file.read(1)
+    while byte:
+        connection.flush()
+
+        print("Python sent: " + bytes(byte))
+        connection.write(bytes(byte))
+        
+        time.sleep(1)
+
+        response = connection.read(connection.inWaiting()) # read all characters in buffer
+        print ("Message from arduino: " + response)
+
         byte = file.read(1)
-        while byte:
-            connection.flush()
-
-            print("Python sent: " + bytes(byte))
-            connection.write(bytes(byte))
-            
-            time.sleep(1)
-
-            response = connection.read(connection.inWaiting()) # read all characters in buffer
-            print ("Message from arduino: " + response)
-
-            byte = file.read(1)
-
 
 connection.close()
