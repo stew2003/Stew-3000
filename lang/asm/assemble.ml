0469a94f35fe62d04d8f1cd427c1adfbb4843620
Thomas Castleman <thomascastleman0@gmail.com>
2021-04-27 20:30:34 -0400

    prep for supporting assemble errors

--- lang/asm/assemble.ml
+++ lang/asm/assemble.ml
@@ -1,5 +1,16 @@
 open Isa
 
+type asm_err =
+  | ProgramTooLarge
+  | DuplicateLabel
+  | InvalidInstr of instr
+  | InvalidImm of immediate
+
+exception AssembleError of asm_err
+
+(* 256 bytes is the size of our program memory *)
+let max_pgrm_size = 256
+
 (* [bytes_from_list] constructs a buffer of raw bytes from
   the given list of integers *)
 let bytes_from_list (l : int list) : bytes =
@@ -7,8 +18,15 @@ let bytes_from_list (l : int list) : bytes =
   List.mapi (fun i b -> Bytes.set_int8 buf i b) l |> ignore;
   buf
 
+(* [validate_imm] checks that an immediate value is within the representable 
+  range [-128, 128), and throws AssembleError InvalidImm if not *)
+let validate_imm (imm : immediate) : immediate =
+  if imm >= -128 && imm < 128 then imm
+  else raise (AssembleError (InvalidImm imm))
+
 (* [assemble_to_list] converts the given instructions to a list of 
-  integers representing the assembled bytes of the program *)
+  integers representing the assembled bytes of the program. Raises 
+  AssembleError InvalidInstr if an un-assemblable instruction is encountered *)
 let assemble_to_list (instrs : instr list) (label_map : (string, int) Hashtbl.t)
     : int list =
   []
@@ -23,6 +41,8 @@ pass over instrs:
     label_to_addr[label] = byte_pos
   otherwise:
     byte_pos += sizeof(ins)
+
+^^ this function can raise AssembleError DuplicateLabel
  *)
 
 (* [size_of] determines the size (in bytes) of the given instruction *)
@@ -45,4 +65,7 @@ let size_of (ins : instr) : int =
 let assemble (instrs : instr list) : bytes =
   let label_map = map_labels instrs in
   let bytes_as_list = assemble_to_list instrs label_map in
-  bytes_from_list bytes_as_list
+  (* check program size to ensure it can fit *)
+  if List.length bytes_as_list > max_pgrm_size then
+    raise (AssembleError ProgramTooLarge)
+  else bytes_from_list bytes_as_list
