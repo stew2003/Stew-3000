ad8652eb48d28744400e914bfcaff40d2e6c5162
stew2003 <stewshadow@gmail.com>
2021-04-28 09:58:01 -0400

    loader code could work if hardware behaves

--- RAM_loader/RAM_loader.ino
+++ RAM_loader/RAM_loader.ino
@@ -5,9 +5,30 @@ int address_pins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 
 RAM_loader loader(address_pins, data_pins, 10);
 
+int instruction_index;
+
 void setup() {
+  Serial.begin(9600); // set the baud rate    
+  Serial.println("Ready"); // print "Ready" once
+
+  instr_index = 0;
 }
 
 void loop() {
+  // delay until data to arrives
+  while (!Serial.available()) {}
+  
+  // serial read section
+  while (Serial.available()) {
+    byte instr = Serial.read();
+    
+    Serial.print("Arduino received: ");
+    Serial.print(instr, HEX);
+    Serial.print(" at ");
+    Serial.println(instr_index, HEX); 
+
+    loader.write_byte(instr_index, instr);
+    instr_index++;
+  }
   
 }
--- RAM_loader/RAM_loader.py
+++ RAM_loader/RAM_loader.py
@@ -1,18 +1,30 @@
 import serial
 import time
+import sys
 
-# send a single byte to the arduino
-def send(connection, byte):
-    connection.write(bytes(byte))
+if __name__ == "main":
+    # set up the serial connection to the arduino board
+    serial_port = "/dev/ttyACM0"
+    baud_rate = "9600"
 
-# receive data from the arduino
-def receive(connection):
-    return connection.read()
+    connection = serial.Serial(serial_port, baud_rate, timeout=5)
 
+    time.sleep(2) # wait for arduino
 
-# set up the serial connection to the arduino board
-serial_port = "/dev/ttyACM0"
-baud_rate = "9600"
-connection = serial.Serial(serial_port, baud_rate)
+    with open(sys.argv[0], "rb") as file:
+        byte = file.read(1)
+        while byte:
+            connection.flush()
+
+            print("Python sent: " + bytes(byte))
+            connection.write(bytes(byte))
+            
+            time.sleep(1)
+
+            response = connection.read(connection.inWaiting()) # read all characters in buffer
+            print ("Message from arduino: " + response)
+
+            byte = file.read(1)
+
 
 connection.close()
