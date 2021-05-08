a047896cf8086a78bf105bb45930a9bf871ade75
Thomas Castleman <thomascastleman0@gmail.com>
2021-05-08 16:18:50 -0400

    add helpful naming utilities for later use

---
+++ lang/util/names.ml
@@ -0,0 +1,13 @@
+(* [gensym] produces a globally unique name, given a base.
+    credit to cs126 for this implementation. *)
+let gensym : string -> string =
+  let counter = ref 0 in
+  fun (base : string) ->
+    let number = !counter in
+    counter := !counter + 1;
+    Printf.sprintf "%s_%d" base number
+
+(* [function_label] produces a unique label for a given
+    function, given its name. *)
+let function_label (base : string) : string =
+  Printf.sprintf "function_%s_%d" base (Hashtbl.hash base)
