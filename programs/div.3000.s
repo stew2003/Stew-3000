653a5a1c8c134d3833a2e45636a43727baf7af4b
Thomas Castleman <thomascastleman0@gmail.com>
2021-05-16 14:10:02 -0400

    both mult & div work with negatives

--- programs/div.3000.s
+++ programs/div.3000.s
@@ -4,6 +4,27 @@ entry:
   call divide
   hlt
 
+; Makes both a and b positive, and sets c to non-zero if 
+; only *one* of a or b was negative. 
+normalize_signs:
+  mvi 0, c
+norm_a:
+  ; if a is negative, make it positive
+  cmpi a, 0
+  jge norm_b
+  not a
+  inr a
+  not c
+norm_b:
+  ; if b is negative, make it positive
+  cmpi b, 0
+  jge done_norm
+  not b
+  inr b
+  not c
+done_norm:
+  ret
+
 ; divide performs integer division of a / b, leaving the
 ; quotient in c, and remainder in a
 ;
@@ -21,39 +42,26 @@ entry:
 ; remainder is left_over
 divide:
   ; normalize signs to positive
-  ; after checks, if c is 0 quotient should be positive,
-  ; and otherwise quotient should be negative
-  mvi 0, c
-check_num:
-  ; if numerator is negative, make it positive
-  cmpi a, 0
-  jge check_denom
-  not a
-  inr a
-  not c
-check_denom:
-  ; if denom is negative, make it positive
-  cmpi b, 0
-  jge after_checks
-  not b
-  inr b
-  not c
-after_checks:
+  call normalize_signs
   sts c, 1 ; store sign of quotient on stack
   mvi 0, c ; now use c as a counter
+
+  ; repeatedly subtract b from a
 loop:
   cmp a, b
-  jl done
+  jl loop_done
   sub b, a
   inr c
   jmp loop
-done:
+loop_done:
   ; quotient in c
   ; remainder in a
+
+  ; load what the sign of quotient should be
   lds 1, b
   cmpi b, 0
-  je return ; if b=0, do not change quotient sign
+  je divide_done
   not c
   inr c
-return:
+divide_done:
   ret
--- programs/mult.3000.s
+++ programs/mult.3000.s
@@ -1,34 +1,74 @@
 entry:
-    mvi 10, b
-    mvi 5, c
+    mvi -1, a
+    mvi -1, b
     call mult
-    out a
+    out c
     hlt
 
-; mult computes the product of b and c, 
-; leaving the result in a
+; Makes both a and b positive, and sets c to non-zero if 
+; only *one* of a or b was negative. 
+normalize_signs:
+  mvi 0, c
+norm_a:
+  ; if a is negative, make it positive
+  cmpi a, 0
+  jge norm_b
+  not a
+  inr a
+  not c
+norm_b:
+  ; if b is negative, make it positive
+  cmpi b, 0
+  jge done_norm
+  not b
+  inr b
+  not c
+done_norm:
+  ret
+
+; mult computes the product of b and c, leaving the result in a
+;
+; :: Implementation :: 
+; running_sum = 0
+; counter = 0
+; while counter != c
+;   counter += 1
+;   running_sum += b
+; product is running_sum
 mult:
-    mvi 0, a
-    sts a, 1 ; running sum
-    sts a, 2 ; counter
+    ; running sum at sp+1
+    ; counter at sp+2
+    ; product sign info at sp+3
+    call normalize_signs
+    sts c, 3
+    mvi 0, c
+    sts c, 2 ; init counter to 0
 loop:
     ; store the running sum, load the counter
-    sts a, 1
-    lds 2, a
+    sts c, 1
+    lds 2, c
 
     ; cmp counter to c
-    cmp a, c
+    cmp c, b
     je done
-    inr a ; increment counter
+    inr c ; increment counter
 
     ; store the counter, load the running sum
-    sts a, 2
-    lds 1, a
+    sts c, 2
+    lds 1, c
 
     ; add b to running sum another time
-    add b, a
+    add a, c
     jmp loop
 done:
     ; restore sum before returning
-    lds 1, a
+    lds 1, c
+
+    ; check what sign of product should be
+    lds 3, a
+    cmpi a, 0
+    je no_sign_change
+    not c
+    inr c
+no_sign_change:
     ret
