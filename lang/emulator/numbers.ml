404b7d7139d1f30ee64cc6ceebade8487f34c784
Thomas Castleman <thomascastleman0@gmail.com>
2021-05-28 18:27:00 -0400

    better 8-bit number emulation with shifting

--- lang/emulator/numbers.ml
+++ lang/emulator/numbers.ml
@@ -1,15 +1,11 @@
+(* How much to shift left in order to erase all bits 
+  other than the rightmost eight *)
+let shift_amount = Sys.int_size - 8
+
 (* [as_8bit_unsigned] interprets an integer as an 8-bit,
   unsigned integer *)
-let as_8bit_unsigned : int -> int =
-  let bytes = Bytes.make 1 '0' in
-  fun (n : int) ->
-    Bytes.set_uint8 bytes 0 n;
-    Bytes.get_uint8 bytes 0
+let as_8bit_unsigned (n : int) : int = (n lsl shift_amount) lsr shift_amount
 
 (* [as_8bit_signed] interprets an integer as an 8-bit,
   signed (two's complement) integer *)
-let as_8bit_signed : int -> int =
-  let bytes = Bytes.make 1 '0' in
-  fun (n : int) ->
-    Bytes.set_int8 bytes 0 n;
-    Bytes.get_int8 bytes 0
+let as_8bit_signed (n : int) : int = (n lsl shift_amount) asr shift_amount
