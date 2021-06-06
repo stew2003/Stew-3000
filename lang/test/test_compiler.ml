cd4ffd2178d818f5e201eb3ab471a09cf560e0b9
Thomas Castleman <thomascastleman0@gmail.com>
2021-06-06 17:09:45 -0400

    refactor compiler tests to use parser

--- lang/test/test_compiler.ml
+++ lang/test/test_compiler.ml
@@ -1,30 +1,45 @@
 open OUnit2
-open Compiler.Compile
-open Compiler.Ast
+open Compiler
 open Emulator
 open Emulator__Machine
-open Util.Srcloc
 
-let dummy_src_loc = loc 0 0
-
-let main_from_body (body : stmt list) : func_defn =
-  { name = "main"; params = []; body; return_ty = Void; loc = dummy_src_loc }
-
-let compile_and_run (pgrm : prog) : stew_3000 =
-  let instrs = compile pgrm in
+(* [run] parses a source program, compiles it, and runs
+  the generated instructions in the emulator, returning
+  the final machine state. *)
+let run (source : string) : stew_3000 =
+  let pgrm = Parser.parse source in
+  let instrs = Compile.compile pgrm in
   emulate instrs
 
-let test_simple_pgrm _ =
-  let machine =
-    compile_and_run
-      {
-        funcs = [];
-        main =
-          main_from_body [ ExprStmt (Num (7, dummy_src_loc), dummy_src_loc) ];
-      }
-  in
+(* [main_from_body] constructs a source string for a main function
+  given the contents of its body. *)
+let main_from_body (body : string) : string =
+  Printf.sprintf "void main() { %s }" body
+
+(* [run_body] generates a program with main() containing the given 
+  body and runs it in the emulator. *)
+let run_body (body : string) : stew_3000 = run (main_from_body body)
+
+let test_expr_stmt _ =
+  let machine = run_body "7;" in
   assert_equal 7 machine.a
 
-let suite = "Compiler Tests" >::: [ "test_simple_pgrm" >:: test_simple_pgrm ]
+let test_let _ =
+  let machine = run_body "int x = 17; x;" in
+  assert_equal 17 machine.a
+
+let test_if_else _ =
+  let machine = run_body "if (1) { 50; } else { 100; }" in
+  assert_equal 50 machine.a;
+  let machine = run_body "if (5 == 17) { 4; } else { 6; }" in
+  assert_equal 6 machine.a
+
+let suite =
+  "Compiler Tests"
+  >::: [
+         "test_expr_stmt" >:: test_expr_stmt;
+         "test_let" >:: test_let;
+         "test_if_else" >:: test_if_else;
+       ]
 
 let () = run_test_tt_main suite
