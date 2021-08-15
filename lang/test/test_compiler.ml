ae433ebc2d110a3ca64398f17cf5429a04af5527
Thomas Castleman <thomascastleman0@gmail.com>
2021-08-15 15:26:43 -0400

    remove dependency on a register from compiler tests

--- lang/test/test_compiler.ml
+++ lang/test/test_compiler.ml
@@ -14,20 +14,16 @@ let run (source : string) : stew_3000 =
   let instrs = Compile.compile pgrm in
   Emulator.emulate instrs
 
-(* [main_from_body] constructs a source string for a main function
-   given the contents of its body. *)
-let main_from_body (body : string) : string =
-  Printf.sprintf "void main() { %s }" body
-
 (* [run_body] generates a program with main() containing the given
    body and runs it in the emulator. *)
-let run_body (body : string) : stew_3000 = run (main_from_body body)
+let run_body (body : string) : stew_3000 =
+  run (Printf.sprintf "void main() { %s }" body)
 
-(* [assert_a] asserts that after running a program with
-   the given body, the machine's a register will equal a given value. *)
-let assert_a (body : string) (a : int) =
-  let machine = run_body body in
-  assert_equal a machine.a ~printer:string_of_int
+(* [assert_expr] asserts that a given expression evaluates to
+   a given value. *)
+let assert_expr (expr : string) (value : int) =
+  let machine = run_body (Printf.sprintf "print(%s);" expr) in
+  assert_equal [ value ] machine.dec_disp_history ~printer:string_of_dec_display
 
 (* [assert_dec] asserts that after running a program with
    the given body, the machine's decimal display history
@@ -37,34 +33,29 @@ let assert_dec (body : string) (dec_history : int list) =
   assert_equal dec_history machine.dec_disp_history
     ~printer:string_of_dec_display
 
-let test_expr_stmt _ =
-  assert_a "7;" 7;
-  assert_a "0;" 0;
-  assert_a "-128;" (-128)
-
 let test_let _ =
-  assert_a "int x = 17; x;" 17;
-  assert_a "int a = 1; int b = a; int c = b; c;" 1
+  assert_dec "int x = 17; print(x);" [ 17 ];
+  assert_dec "int a = 1; int b = a; int c = b; print(c);" [ 1 ]
 
 let test_if _ =
-  assert_a "100; if (1) { 17; }" 17;
-  assert_a "int x = 100; if (0) { x = 17; } x;" 100
+  assert_dec "if (1) { print(17); }" [ 17 ];
+  assert_dec "int x = 100; if (0) { x = 17; } print(x);" [ 100 ]
 
 let test_if_else _ =
-  assert_a "if (1) { 50; } else { 100; }" 50;
-  assert_a "if (5 == 17) { 4; } else { 6; }" 6
+  assert_dec "if (1) { print(50); } else { print(100); }" [ 50 ];
+  assert_dec "if (5 == 17) { print(4); } else { print(6); }" [ 6 ]
 
 let test_block _ =
-  assert_a "{ { { 90; } 80; } 70; }" 70;
-  assert_a "10; {} {}" 10
+  assert_dec "{ { { print(90); } print(80); } print(70); }" [ 90; 80; 70 ];
+  assert_dec "{} print(10); {} {}" [ 10 ]
 
 let test_prefix_inr _ =
   assert_dec "int x = 14; print(++x);" [ 15 ];
   assert_dec "int x = 0; int *px = &x; print(++(*px)); print(x);" [ 1; 1 ]
 
 let test_postfix_inr _ =
-  assert_a "int x = 50; x++; x++; x;" 52;
-  assert_a "int y = -16; y++; y;" (-15);
+  assert_dec "int x = 50; x++; x++; print(x);" [ 52 ];
+  assert_dec "int y = -16; y++; print(y);" [ -15 ];
   assert_dec "int x = 5; int *px = &x; *(px)++; print(x);" [ 6 ];
   assert_dec "int x = 14; print(x++);" [ 14 ]
 
@@ -73,8 +64,8 @@ let test_prefix_dcr _ =
   assert_dec "int x = 120; int *px = &x; print(--(*px)); print(x);" [ 119; 119 ]
 
 let test_postfix_dcr _ =
-  assert_a "int a = 71; a--; a;" 70;
-  assert_a "int b = -100; b--; b--; b;" (-102);
+  assert_dec "int a = 71; a--; print(a);" [ 70 ];
+  assert_dec "int b = -100; b--; b--; print(b);" [ -102 ];
   assert_dec "int x = 14; int *px = &x; *(px)--; print(x);" [ 13 ];
   assert_dec "int x = 71; print(x--);" [ 71 ]
 
@@ -93,98 +84,98 @@ let test_while _ =
 
 let test_nums _ =
   (* decimal literals *)
-  assert_a "41;" 41;
-  assert_a "-120;" (-120);
+  assert_expr "41" 41;
+  assert_expr "-120" (-120);
   (* hex literals *)
-  assert_a "0xa;" 0xa;
-  assert_a "-0x04;" (-0x04);
+  assert_expr "0xa" 0xa;
+  assert_expr "-0x04" (-0x04);
   (* binary literals *)
-  assert_a "0b10110;" 0b10110;
-  assert_a "-0b110101;" (-0b110101)
+  assert_expr "0b10110" 0b10110;
+  assert_expr "-0b110101" (-0b110101)
 
 let test_unops _ =
   (* bitwise not *)
-  assert_a "~0b00001011;" 0b11110100;
-  assert_a "~0b00000110;" 0b011111001
+  assert_expr "~0b00001011" 0b11110100;
+  assert_expr "~0b00000110" 0b011111001
 
 let test_binops _ =
   (* addition *)
-  assert_a "2 + 3;" 5;
-  assert_a "5 + 10 + 15;" 30;
+  assert_expr "2 + 3" 5;
+  assert_expr "5 + 10 + 15" 30;
 
   (* subtraction *)
-  assert_a "4 - 7;" (-3);
-  assert_a "10 - 9 - 3;" (-2);
+  assert_expr "4 - 7" (-3);
+  assert_expr "10 - 9 - 3" (-2);
 
   (* multiplication *)
-  assert_a "7 * 6;" 42;
-  assert_a "-10 * 5 * 2;" (-100);
+  assert_expr "7 * 6" 42;
+  assert_expr "-10 * 5 * 2" (-100);
 
   (* division *)
-  assert_a "100 / 25;" 4;
-  assert_a "17 / -6;" (-2);
+  assert_expr "100 / 25" 4;
+  assert_expr "17 / -6" (-2);
 
   (* modulus *)
-  assert_a "40 % 10;" 0;
-  assert_a "21 % 50;" 21;
-  assert_a "10 % 4;" 2;
+  assert_expr "40 % 10" 0;
+  assert_expr "21 % 50" 21;
+  assert_expr "10 % 4" 2;
 
   (* bitwise and *)
-  assert_a "0b1100 & 0b0101;" 0b0100;
-  assert_a "0b0110 & 0b11;" 0b10;
+  assert_expr "0b1100 & 0b0101" 0b0100;
+  assert_expr "0b0110 & 0b11" 0b10;
 
   (* bitwise or *)
-  assert_a "0b1101 | 0b1010;" 0b1111;
-  assert_a "0b00001111 | 0b00101101;" 0b00101111;
+  assert_expr "0b1101 | 0b1010" 0b1111;
+  assert_expr "0b00001111 | 0b00101101" 0b00101111;
 
   (* bitwise xor *)
-  assert_a "0b1011 ^ 0b1110;" 0b0101;
-  assert_a "0b1111 ^ 0b0000;" 0b1111;
+  assert_expr "0b1011 ^ 0b1110" 0b0101;
+  assert_expr "0b1111 ^ 0b0000" 0b1111;
 
   (* greater than *)
-  assert_a "50 > 32;" 1;
-  assert_a "-9 > 9;" 0;
-  assert_a "14 > 14;" 0;
+  assert_expr "50 > 32" 1;
+  assert_expr "-9 > 9" 0;
+  assert_expr "14 > 14" 0;
 
   (* less than *)
-  assert_a "100 < 120;" 1;
-  assert_a "16 < -2;" 0;
-  assert_a "88 < 88;" 0;
+  assert_expr "100 < 120" 1;
+  assert_expr "16 < -2" 0;
+  assert_expr "88 < 88" 0;
 
   (* greater than or eq *)
-  assert_a "14 >= 12;" 1;
-  assert_a "-4 >= 0;" 0;
-  assert_a "79 >= 79;" 1;
+  assert_expr "14 >= 12" 1;
+  assert_expr "-4 >= 0" 0;
+  assert_expr "79 >= 79" 1;
 
   (* less than or eq *)
-  assert_a "7 <= 21;" 1;
-  assert_a "-44 <= -127;" 0;
-  assert_a "-9 <= -9;" 1;
+  assert_expr "7 <= 21" 1;
+  assert_expr "-44 <= -127" 0;
+  assert_expr "-9 <= -9" 1;
 
   (* equal *)
-  assert_a "45 == 45;" 1;
-  assert_a "-100 == 18;" 0;
+  assert_expr "45 == 45" 1;
+  assert_expr "-100 == 18" 0;
 
   (* not equal *)
-  assert_a "16 != 90;" 1;
-  assert_a "47 != 47;" 0
+  assert_expr "16 != 90" 1;
+  assert_expr "47 != 47" 0
 
 let test_log_ops _ =
   (* logical and *)
-  assert_a "10 && 2;" 2;
-  assert_a "0 && 17;" 0;
-  assert_a "1 && 2 && 0 && 3;" 0;
-  assert_a "-1 && 18 && 44 && 11;" 11;
+  assert_expr "10 && 2" 2;
+  assert_expr "0 && 17" 0;
+  assert_expr "1 && 2 && 0 && 3" 0;
+  assert_expr "-1 && 18 && 44 && 11" 11;
 
   (* logical or *)
-  assert_a "18 || 15;" 18;
-  assert_a "100 || 0;" 100;
-  assert_a "0 || 0 || 3 || 0;" 3;
-  assert_a "0 || 0 || 0;" 0;
+  assert_expr "18 || 15" 18;
+  assert_expr "100 || 0" 100;
+  assert_expr "0 || 0 || 3 || 0" 3;
+  assert_expr "0 || 0 || 0" 0;
 
   (* logical not *)
-  assert_a "!17;" 0;
-  assert_a "!0;" 1
+  assert_expr "!17" 0;
+  assert_expr "!0" 1
 
 let test_functions _ =
   let machine =
@@ -231,83 +222,73 @@ let test_ignore_asserts _ =
        "void main() { assert(1 == 2); assert(1); assert(41 - 1 == 40); }")
 
 let test_char_literals  _ = 
-  let machine = run_body "
+  assert_dec "
       char a = 'A';
-      print((int)a);" in 
-  assert_equal ~printer:string_of_dec_display [65] machine.dec_disp_history;
-  let machine = run_body "
+      print((int)a);" [65];
+  assert_dec "
       char c = '[';
       if (c + 2 == ']') {
         print(1);
       } else {
         print(0);
-      }" in 
-  assert_equal ~printer:string_of_dec_display [1] machine.dec_disp_history
+      }" [1]
   [@@ocamlformat "disable"]
 
 let test_deref _ = 
-  let machine = run_body "
+  assert_dec "
     int x = 41;
     int *px = &x;
     print(*px);
-  " in 
-  assert_equal ~printer:string_of_dec_display [41] machine.dec_disp_history;
-  let machine = run_body "
+  " [41];
+  assert_dec "
     int x = -3;
     int *px = &x;
     int **ppx = &px;
     print(**ppx);
-  " in 
-  assert_equal ~printer:string_of_dec_display [-3] machine.dec_disp_history; 
-  let machine = run_body "
+  " [-3]; 
+  assert_dec "
     *(unsigned*)(1 + 2) = 255;
     print(*(int*)3);
-  " in 
-  assert_equal ~printer:string_of_dec_display [255] machine.dec_disp_history
+  " [255]
   [@@ocamlformat "disable"]
 
 let test_addr_of _ = 
-  let machine = run_body "
+  assert_dec "
     int x;
     print(&x);
-  " in 
-  assert_equal ~printer:string_of_dec_display [1] machine.dec_disp_history;
-  let machine = run_body "
+  " [1];
+  assert_dec "
     int x;
     int *px;
     print(&*px == &x);
     print(&*(px + 1) == &x + 1);
-  " in 
-  assert_equal ~printer:string_of_dec_display [1; 1] machine.dec_disp_history
+  " [1; 1]
   [@@ocamlformat "disable"]
 
 let test_assign _ = 
-  let machine = run_body "
+  assert_dec "
     int a = 10;
     *(&a) = 41;
     print(a);
-  " in 
-  assert_equal ~printer:string_of_dec_display [41] machine.dec_disp_history;
-  let machine = run_body "
+  " [41];
+  assert_dec "
     char c = 'v';
     c = 'z';
     c = 'B';
     print((int)c);
-  " in 
-  assert_equal ~printer:string_of_dec_display [66] machine.dec_disp_history
+  " [66]
   [@@ocamlformat "disable"]
 
 let test_array_decl _ = 
-  let machine = run_body "
+  assert_dec "
     int arr[5] = { 2, 4, 6, 8, 10 };
     print(*(arr + 0));
     print(*(arr + 1));
     print(*(arr + 2));
     print(*(arr + 3));
     print(*(arr + 4));
-  " in 
-  assert_equal ~printer:string_of_dec_display [2; 4; 6; 8; 10] machine.dec_disp_history;
-  let machine = run_body "
+  " [2; 4; 6; 8; 10];
+  assert_dec "
     char str[] = \"hello\";
     print((int)*(arr + 0));
     print((int)*(arr + 1));
@@ -315,14 +296,12 @@ let test_array_decl _ =
     print((int)*(arr + 3));
     print((int)*(arr + 4));
     print((int)*(arr + 5));
-  " in 
-  assert_equal ~printer:string_of_dec_display [104; 101; 108; 108; 111; 0;] machine.dec_disp_history
+  " [104; 101; 108; 108; 111; 0;]
   [@@ocamlformat "disable"]
 
 let suite =
   "Compiler Tests"
   >::: [
-         "test_expr_stmt" >:: test_expr_stmt;
          "test_let" >:: test_let;
          "test_if" >:: test_if;
          "test_if_else" >:: test_if_else;
