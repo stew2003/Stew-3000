843a6be4c4b81998dc888e0e2a2f2413f255fabe
Stewart Morris <37306424+stew2003@users.noreply.github.com>
2021-08-24 12:05:32 -0400

    Immediate optimizations (#25)
    * first draft of using more immediate instructions
    
    * refactor the iuses of immediate instructions
    
    * add more logging to compiler + disable optimizations flag
    
    * up version numbers

--- lang/bin/compile.ml
+++ lang/bin/compile.ml
@@ -6,6 +6,7 @@ open Asm.Warnings
 open Compiler.Optimizations
 open Compiler.Warnings
 open Err
+open Util
 
 (* command-line interface for compiler *)
 let command =
@@ -18,6 +19,8 @@ let command =
           ~doc:"binary_file emit compiled binary to file"
       and ignore_asserts =
         flag "-ignore-asserts" no_arg ~doc:"do not generate code for asserts"
+      and disable_opt =
+        flag "-disable-opt" no_arg ~doc:"do not apply extra optimizations"
       in
       fun () ->
         (* read input file into string *)
@@ -27,32 +30,38 @@ let command =
           let pgrm = Preprocess.preprocess pgrm in
           let pgrm = Desugar.desugar pgrm in
 
-          Printf.printf "After desugar:\n%s\n" (Prettyprint.pretty_print pgrm);
-
           (* On warnings, print them *)
           let warning_handler (w : compiler_warn) =
             print_warning (message_of_compiler_warn w source_text src_file)
           in
 
           let pgrm = Check.check ~emit_warning:warning_handler pgrm in
-          let pgrm =
-            Constant_fold.constant_fold ~emit_warning:warning_handler pgrm
-          in
-          let pgrm =
-            Dead_code_elimination.eliminate_dead_code
-              ~emit_warning:warning_handler pgrm
-          in
 
-          Printf.printf "After optimizing AST:\n%s\n"
-            (Prettyprint.pretty_print pgrm);
+          let pgrm =
+            if disable_opt then pgrm
+            else Constant_fold.constant_fold ~emit_warning:warning_handler pgrm
+          in
+          let pgrm =
+            if disable_opt then pgrm
+            else
+              Dead_code_elimination.eliminate_dead_code
+                ~emit_warning:warning_handler pgrm
+          in
 
           let instrs = Compile.compile pgrm ~ignore_asserts in
 
           (* optimize the generated instructions *)
-          let instrs = Peephole.peephole_optimize instrs in
+          let instrs =
+            if disable_opt then instrs else Peephole.peephole_optimize instrs
+          in
 
           (* write generated asm to target file *)
           Out_channel.write_all target_file ~data:(string_of_instr_list instrs);
+
+          (* print message about generated code *)
+          Printf.printf "%s `%s` ==> `%s` (%d instructions)\n"
+            (Colors.success "Success!")
+            src_file target_file (List.length instrs);
 
           (* write binary also if requested *)
           match emit_binary with
@@ -65,7 +74,13 @@ let command =
               in
               let bin_out = Out_channel.create bin_file in
               Out_channel.output_bytes bin_out assembled;
-              Out_channel.close bin_out
+              Out_channel.close bin_out;
+
+              (* print message about assembled binary *)
+              Printf.printf "%s `%s` (%d instructions) ==> `%s` (%d bytes)\n"
+                (Colors.success "Success!")
+                target_file (List.length instrs) bin_file
+                (Bytes.length assembled)
         with err -> handle_err err source_text src_file)
 
-let () = Command.run ~version:"1.0" command
+let () = Command.run ~version:"1.1.0" command
