0806f4bf1f07e25c01f0f5f43fc176dd7e047979
stew2003 <stewshadow@gmail.com>
2021-08-27 10:00:35 -0400

    beginning work on itoa

---
+++ lang/compiler/runtime/itoa.3000.s
@@ -0,0 +1,43 @@
+; runtime_itoa converts an integer in register A, 
+; to a string to be stored in a buffer pointed to by register B
+;
+; :: Implementation ::
+; char* itoa (int num, char* str) {
+;   int cur = 0;
+;   int isNeg = 0;
+;
+;   if (num == 0) {
+;       str[cur++] = '0';
+;       str[cur] = '\0'; 
+;       return str;
+;   }
+;
+;   if (num <  0) {
+;       isNeg = 1;
+;       num = -num; 
+;   }
+;
+;   while (num != 0) {
+;       int digit = num % 10;
+;       str[cur++] = 48 + digit;
+;       num = num/base;
+;   }
+;
+;   if (isNeg) {
+;       str[cur++] = '-';   
+;   }
+;   str[cur] = '\0';
+;
+;   int start = 0;
+;   int end = --cur;
+;
+;   while (start < end) {
+;       int temp = str[start];
+;       str[start++] = str[end];
+;       str[end--] = temp;
+;   }
+;
+;   return str;
+; }
+runtime_itoa:
+    sts b, 1;
---
+++ programs/source/scope.3000.c
@@ -0,0 +1,18 @@
+char* itoa (int num, char* str) {
+   int cur = 0;
+   int isNeg = 0;
+
+   if (num < 0) {
+       isNeg = 1;
+   }
+
+    print(isNeg);
+
+   return str;
+}
+
+void main() {
+    char buffer[5];
+    itoa(-3, buffer);
+    print_lcd(buffer);
+}
